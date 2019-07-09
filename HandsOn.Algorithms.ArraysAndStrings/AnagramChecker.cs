b5680c08a53b02fa0205ab12c41de0eea7016220
Samuele Resca <samuele.resca@just-eat.com>
2019-07-09 09:43:15 +0100

    Updating problems

---
+++ HandsOn.Algorithms.ArraysAndStrings/AnagramChecker.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Linq;
+
+namespace HandsOn.Algorithms.ArraysAndStrings
+{
+    public class AnagramChecker
+    {
+        public bool IsAnagram(string s, string t)
+        {
+
+            if (s.Length != t.Length) return false;
+
+            var count1 = new int[256];
+            var count2 = new int[256];
+
+            for (var i = 0; i < s.Length && i < t.Length;
+                i++)
+            {
+                count1[s[i]]++;
+                count2[t[i]]++;
+            }
+
+
+            for (int i = 0; i < count1.Length; i++)
+            {
+                if (count1[i] != count2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        public bool IsAnagramOlogN(string s, string t)
+        {
+            // Get lenghts of both strings 
+            int n1 = s.Length;
+            int n2 = t.Length;
+
+            // If length of both strings is not 
+            // same, then they cannot be anagram 
+            if (n1 != n2)
+            {
+                return false;
+            }
+
+            var sArr = s.ToCharArray();
+            var tArr = t.ToCharArray();
+            // Sort both strings 
+            Array.Sort(sArr);
+            Array.Sort(tArr);
+
+            // Compare sorted strings 
+            for (int i = 0; i < n1; i++)
+            {
+                if (sArr[i] != tArr[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ HandsOn.Algorithms.ArraysAndStrings/WordGrid.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace HandsOn.Algorithms.ArraysAndStrings
+{
+    public class WordGrid
+    {
+        public bool Exist(char[][] board, string word)
+        {
+
+            var rowLength = board.GetLength(0);
+            var columnLength = board[0].Length;
+
+            for (int rowIndex = 0; rowIndex < rowLength; rowIndex++)
+            {
+
+                for (int columnIndex = 0; columnIndex < columnLength; columnIndex++)
+                {
+                    if (search2D(board, rowIndex, columnIndex, word))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool search2D(char[][] grid, int row,
+            int col, string word)
+        {
+
+            var x = new int[] { -1, 0, 1, 0 };
+            var y = new int[] { 0, -1, 0, 1 };
+
+            if (grid[row][col] != word[0])
+                return false;
+
+            for (int dir = 0; dir < 4; dir++)
+            {
+                int k, rd = row + x[dir], cd = col + y[dir];
+
+                for (k = 1; k < word.Length; k++)
+                {
+                    if (rd >= grid.GetLength(0) || rd < 0 || cd >= grid[0].Length || cd < 0)
+                        break;
+
+                    if (grid[rd][cd] != word[k])
+                        break;
+
+                    rd += x[dir];
+                    cd += y[dir];
+                }
+
+                if (k == word.Length - 1)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
